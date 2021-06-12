0a39fc1579a24fa53e68011d72c9b15170bf653d
Rnagao <nagao1234@poli.ufrj.br>
2021-06-12 16:40:19 -0300

    tirando / do final do endpoints

--- app/questoes/routes.py
+++ app/questoes/routes.py
@@ -4,13 +4,13 @@ from .controllers import QuestoesDetail, QuestoesList, QuestaoAleatoria
 questoes_api = Blueprint('questoes_api', __name__)
 
 questoes_api.add_url_rule(
-    '/questoes/', view_func=QuestoesList.as_view('questoes_list'), methods=['POST']
+    '/questoes', view_func=QuestoesList.as_view('questoes_list'), methods=['POST']
 )
 
 questoes_api.add_url_rule(
-    '/questoes/<int:numero_questao>/', view_func=QuestoesDetail.as_view('questoes_detail'), methods=['GET', 'DELETE', 'POST']
+    '/questoes/<int:numero_questao>', view_func=QuestoesDetail.as_view('questoes_detail'), methods=['GET', 'DELETE', 'POST']
 )
 
 questoes_api.add_url_rule(
-    '/questoes/random/', view_func=QuestaoAleatoria.as_view('questoes_aleatorias'), methods=['GET']
+    '/questoes/random', view_func=QuestaoAleatoria.as_view('questoes_aleatorias'), methods=['GET']
 )
