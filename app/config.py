40556c9f9c503d517d3d5ca1a085604dbe20aa1d
Rnagao <nagao1234@poli.ufrj.br>
2021-06-12 03:50:24 -0300

    secret key

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 from os import environ
 
 class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
