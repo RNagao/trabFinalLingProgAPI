2040221fdd89f9ebe2677b299076030a18614a04
Rnagao <nagao1234@poli.ufrj.br>
2021-06-12 05:14:42 -0300

    deploy

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from os import environ
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = 'postgres://ysefkhnhdyxsjl:f5560af554c1350621b22b26f75e05b922924457e1a42b8979b77d6589ce09f2@ec2-54-224-194-214.compute-1.amazonaws.com:5432/d3s9k15u6pqorm'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
